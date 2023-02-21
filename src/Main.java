5161245404de0ea0c3b103080b63d74f0347e195
AlejandroGasca09 <119777270+AlejandroGasca09@users.noreply.github.com>
2023-02-21 03:05:44 -0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner teclado = new Scanner(System.in);
+        System.out.print(" Escribe tu nombre , profesion y pais de origen");
+        String entrada = teclado.nextLine();
+        String nombre = teclado.nextLine();
+        String profesion = teclado.nextLine();
+        String pais = teclado.nextLine();
+        System.out.println("Nombre :"+nombre.toUpperCase());
+        System.out.println("Profesión: "+ profesion.toUpperCase());
+        System.out.println("País: "+ pais.toUpperCase());
     }
 }
